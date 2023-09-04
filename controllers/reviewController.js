d9f242c72ef5a234ab96075720511ba7f0e8599f
diegocolonia <diegocolonia98@yahoo.com>
2023-09-04 11:47:35 -0400

    commit

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -67,7 +67,18 @@ const updateReview =async(req,res)=>{
 }
 
 const deleteReview = async(req,res)=>{
-    res.send('delete review');
+    
+    const {id:reviewId} = req.params;
+
+    const review = await Review.findOne({_id:reviewId});
+
+    if(!review){
+        throw new CustomError.NotFoundError(`no review found to delete with id ${reviewId}`);
+    }
+
+    await review.remove();
+
+    res.status(StatusCodes.OK).json({review});
 }
 
 
