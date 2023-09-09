73c8b12c11f4c6c43670e152f0d5272b41524f12
diegocolonia <diegocolonia98@yahoo.com>
2023-09-08 23:25:51 -0400

    commit

--- models/Review.js
+++ models/Review.js
@@ -57,7 +57,7 @@ ReviewSchema.statics.calculateAverageRating = async function(productId){
             {_id:productId},
             {
                 averageRating:Math.ceil(result[0]?.averageRating || 0),
-                numOfReviews:result[0]?.averageRating || 0,
+                numOfReviews:result[0]?.numOfReviews || 0,
             }
          );
  
